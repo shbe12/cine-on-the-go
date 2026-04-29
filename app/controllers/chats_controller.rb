3bb5ef46bf580b86ccee4c6c34d24c8653a156b1
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2026-04-28 23:51:12 -0400

    added chat controller

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,28 @@
+class ChatsController < ApplicationController
+  def index
+    @chats = Chat.all
+  end
+
+  def show
+    @chat = Chat.find(params[:id])
+  end
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to @chat, notice: 'Chat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:name)
+  end
+end
